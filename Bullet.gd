a554bb09b0779b2f2300565c7039aaeeb74d24d6
KFoster23 <77758593+KFoster23@users.noreply.github.com>
2021-02-14 21:08:44 -0500

    Completes Exercise

--- Bullet.gd
+++ Bullet.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 onready var VP = get_viewport().size
 onready var Explosions = get_node("/root/Game/Explosions")
-var Explosion = load("res://Explosion/Explosion.tscn")
+var Explosion = load("res://Explosions/Explosion.tscn")
 
 export var Velocity = Vector2(0,-1)
 export var Acceleration = Vector2(0,-1)
@@ -18,8 +18,4 @@ func _physics_process(_delta):
 
 
 func _on_Bullet_body_entered(body):
-	var explosion = Explosion.instance()
-	explosion.global_position = global_position
-	Explosions.add_child(explosion)
-	body.queue_free()
-	queue_free()
+	pass # Replace with function body.
